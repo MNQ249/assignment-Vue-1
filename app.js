0a9af6c6b0721c7cac8fab5f5dffa287d4c910d7
Mesfer <mesfer.vip@gmail.com>
2020-10-14 11:59:00 +0300

    add name + age + age after 5 years + image

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+
+data(){
+    return{
+        myName: 'Mesfer',
+        myAge: 30,
+        // myAgeAfterFiveYear: '35'
+       
+    }
+},
+methods:{
+result(){
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+}
+
+});
+
+app.mount('#assignment');
